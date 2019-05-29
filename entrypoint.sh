14652bf882abad142e948be279a5632f87384ae4
Denis Kramer <d.kramer@soton.ac.uk>
2019-05-29 17:25:47 +0100

    changed collection method to piped tar via ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,9 @@ fi
 ssh -i /ssh/id_rsa -oStrictHostKeyChecking=no $USERNAME@$HOSTNAME "rm /scratch/$USERNAME/$BASEDIR/$JOB_NAME/vasp.* /scratch/$USERNAME/$BASEDIR/$JOB_NAME/qscript"
 
 # Copy the input files
-scp -i /ssh/id_rsa -oStrictHostKeyChecking=no -r $USERNAME@$HOSTNAME:/scratch/$USERNAME/$BASEDIR/$JOB_NAME/* /data/vasp/*
+# We pipe this through a tar command because it allows to write files to a folder without wildcards (which are problematic in scp)
+mkdir -p /data/vasp
+ssh -i /ssh/id_rsa -oStrictHostKeyChecking=no -r $USERNAME@$HOSTNAME tar czf - -C /scratch/$USERNAME/$BASEDIR/$JOB_NAME . | tar xzf - -C /data/vasp
 
 # Delete files from server
 # ssh -i /ssh/id_rsa -oStrictHostKeyChecking=no $USERNAME@$HOSTNAME "rm -rf /scratch/$USERNAME/$BASEDIR/$JOB_NAME"
